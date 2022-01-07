6d2a7bbc3bfff81e786dcd8aa430a641dad0d872
Alexandr Kokorin <Akokorin89@yandex.ru>
2022-01-07 14:36:39 +0300

    Добавлен конвертор

---
+++ Praktikum.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Praktikum {
+    public static void main(String[] args) {
+
+     class   Converter {
+         
+     }
